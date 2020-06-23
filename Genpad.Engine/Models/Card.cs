6da31316df128bb8c9dda8ad85f066d4bba9f5d1
Jeremy Willhelm <jtwillhelm@gmail.com>
2020-06-23 19:43:04 -0400

    added engine and unit test projects

---
+++ Genpad.Engine/Models/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Genpad.Engine.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Note { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
