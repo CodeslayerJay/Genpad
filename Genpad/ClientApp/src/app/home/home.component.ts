41f354d64a62a63e610dd91c79e9e43f5f9fe042
Jeremy Willhelm <jtwillhelm@gmail.com>
2020-06-24 19:52:53 -0400

    fixed styling issue

--- Genpad/ClientApp/src/app/home/home.component.ts
+++ Genpad/ClientApp/src/app/home/home.component.ts
@@ -6,27 +6,6 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  model = new CardTest("","");
-  _http: HttpClient;
-  _url: string;
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) { 
-    this._http = http;
-    this._url = baseUrl + 'api/Cards';
-  }
-
-  save() {
-    this._http.post(this._url, this.model).subscribe(result => {
-      console.log(result);
-    }, error => console.error(error));
-  }
-}
-
-
-export class CardTest {
-  constructor(
-    public title: string,
-    public note: string
-    ) {}
 }
 
